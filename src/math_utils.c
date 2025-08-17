03ec0748b37341a67cf53f53ed837c9c3cda007c
osboxes.org <sirmercerfrey@gmail.com>
2025-08-17 09:14:29 -0400

    yeah

--- src/math_utils.c
+++ src/math_utils.c
@@ -32,44 +32,15 @@ t_complex	abs_complex(t_complex z)
 	return (result);
 }
 
-/*int	get_psychedelic_color(int i, t_fractal *fractal)
-{
-	double	t;
-	int		r;
-	int		g;
-	int		b;
-
-	t = (double)i / fractal->iterations_definition;
-	r = (int)(9 * (1 - t) * t * t * t * 255);
-	g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
-	b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
-	if (1 == fractal->color_shift % 3)	
-		return ((b << 16) | (r << 8) | g);
-	else if (2 == fractal->color_shift % 3)
-		return ((g >> 16) | (b >> 8) | r);
-	else
-		return ((r << 16) | (g << 8) | b);
-}
-*/
-
 int	get_psychedelic_color(int i, t_fractal *fractal)
 {
-	int		r;
-	int		g;
-	int		b;
-	int		colors[3];
+	int		cross;
 	int		shift;
-	double	t;
 
-	t = (double)i / fractal->iterations_definition;
-	r = (int)(sin(6.283 * t + 0) * 127 + 128);
-	g = (int)(sin(6.283 * t + 2) * 127 + 128);
-	b = (int)(sin(6.283 * t + 4) * 127 + 128);
-	colors[0] = r;
-	colors[1] = g;
-	colors[2] = b;
-	shift = fractal->color_shift % 3;
-	return ((colors[(shift + 0) % 3] << 16)
-			| (colors[(shift + 1) % 3] << 8)
-			| colors[(shift + 2) % 3]);
+	if (fractal->iterations_definition == 0)
+		return (COLORS[0]);
+	
+	cross = i * COLOR_COUNT / fractal->iterations_definition;
+	shift = fractal->color_shift % COLOR_COUNT;
+	return (COLORS[(cross + shift) % COLOR_COUNT]);
 }
